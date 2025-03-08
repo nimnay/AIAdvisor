e3129024ceb44da205c474eaeb3f046b01c640ff
Hannah Leach <hleach@clemson.edu>
2025-03-08 11:36:44 -0500

    changed student model

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from utils import callAPI
 if __name__ == '__main__':
     print("Enter your name: ")
     name = input()
-    print("Enter your grade: ")
-    grade = input()
+    print("Enter classes already taken with a space in between each: ")
+    classes = input()
     print("Enter any time constraints: ")
     time_constraints = input()
     student = Student(name, grade, time_constraints)
