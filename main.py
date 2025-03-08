38cee2094b416e1932a19243d2ee5f0f27d35351
Nimra Nayyar <nimranayyar05@gmail.com>
2025-03-08 11:39:27 -0500

    edited classes and time constraints

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from utils import callAPI
 if __name__ == '__main__':
     print("Enter your name: ")
     name = input()
-    print("Enter classes already taken with a space in between each: ")
+    print("Enter classes already taken with a space in between each: e.g CPSC 1010, ENG 1030")
     classes = input()
-    print("Enter any time constraints: ")
+    print("Enter any time slot constraints: e.g. 3-4pm")
     time_constraints = input()
     student = Student(name, grade, time_constraints)
 
