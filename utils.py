2bf8e17fca81eef491ab24a925b4c79f222d1a3c
Hannah Leach <hleach@clemson.edu>
2025-03-08 13:43:22 -0500

    finished utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import boto3
 import json
 
+from botocore.exceptions import ClientError
+
+
 def callAPI(student):
     kb_id = "IIPMMYP0DR"
 
@@ -24,10 +27,10 @@ def callAPI(student):
                   )
 
     response = retrieveAndGenerate(input_text, kb_id)
-    "createList(response)"
+    answer = createList(response)
 
-    if response:
-        return response
+    if answer:
+        return answer
     else:
         print("No response received from AWS Bedrock.")
 
@@ -48,7 +51,6 @@ def retrieveAndGenerate(input_text, kb_id):
                 "modelArn": "arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0"
             }
         },
-        #"generationPrompt": hard_coded_prompt,
     }
 
     if session_id:
@@ -64,4 +66,41 @@ def retrieveAndGenerate(input_text, kb_id):
         print(f"Error retrieving and generating response: {e}")
         return None
 
+def createList(response):
+    client = boto3.client("bedrock-runtime", region_name="us-west-2")
+    model_id = "arn:aws:bedrock:us-west-2:363793501045:inference-profile/us.meta.llama3-2-1b-instruct-v1:0"
+    prompt = f"Create a list from this data providing the class numbers class times with each class's data on a separate line: {response}"
+    formatted_prompt = f"""
+    <|begin_of_text|><|start_header_id|>user<|end_header_id|>
+    {prompt}
+    <|eot_id|>
+    <|start_header_id|>assistant<|end_header_id|>
+    """
 
+    native_request = {
+        "prompt": formatted_prompt,
+        "max_gen_len": 512,
+        "temperature": 1,
+        "top_p": 0.1,
+    }
+
+    request = json.dumps(native_request).encode('utf-8')
+
+    try:
+        response = client.invoke_model(
+            modelId=model_id,
+            body=request,
+            contentType="application/json",
+            accept="application/json"
+        )
+
+        model_response = json.loads(response["body"].read())
+
+        response_text = model_response["generation"]
+        return response_text
+
+    except ClientError as e:
+        print(f"AWS ClientError: {e}")
+    except Exception as e:
+        print(f"General error: {e}")
+        raise
